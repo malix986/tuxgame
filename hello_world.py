9e4014fe476ffd3985ca3a8fb0a6e9d57e2dbeba
Andrea Malinverni <amalinverni@google.com>
2020-09-16 23:26:31 +0200

    sql test

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,18 @@
-print('hello worlald')
+import mysql_functions
+import settings
+
+print('hello works')
+
+settings.init()
+
+db = settings.db
+cursor = settings.cursor
+## Prepare SQL query to INSERT the new record into the database.
+print('cursor set')
+sql = 'insert into test_table(stringa) VALUES ("pluto")'
+# Execute the SQL command
+cursor.execute(sql)
+print('cursor executed')
+# Commit your changes in the database
+db.commit()
+print('cursor committed \n')
