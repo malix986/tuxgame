3ce1f1ec6da5530f751a8903f0899c3af586ed6e
Andrea Malinverni <amalinverni@google.com>
2020-09-22 23:19:12 +0200

    lighter get id

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ client = bigquery.Client()
 #     unique_words = row['unique_words']
 #     print(f'{title:<20} | {unique_words}')
 
+
 def get_character_id(character_name):
-   query = 'SELECT ROW_NUMBER() OVER() AS id FROM character_list WHERE name = "' + str(character_name) + '"'
+   query = 'SELECT id from (SELECT name, ROW_NUMBER() OVER() AS id FROM tuxgame.character_list) WHERE name = "' + str(character_name) + '"'
    print('  getting character id')
    # Execute the SQL command
    result = client.query(query)
    print('SQL executed')
    # Fetch all the rows in a list of lists.
-   
-   print('character_id: '+str(result[0]['id'])+'\n')
-   character_id = result[0]['id']
+   for row in result:
+       character_id = row['id']
+       print('character_id: '+str(character_id)+'\n')
    return character_id
 
-print(get_character_id('lighter'))
+print(get_character_id('paperino'))
